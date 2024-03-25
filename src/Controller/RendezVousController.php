aa56b46af8fb1c0b0ed624dc3c88e075d0f1d19c
MarieEchard <marie.echard79@gmail.com>
2024-03-25 17:41:29 +0100

    Creation des différents controller

---
+++ src/Controller/RendezVousController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RendezVousController extends AbstractController
+{
+    #[Route('/rendez/vous', name: 'app_rendez_vous')]
+    public function index(): Response
+    {
+        return $this->render('rendezVous.html.twig', [
+            'controller_name' => 'RendezVousController',
+        ]);
+    }
+}
