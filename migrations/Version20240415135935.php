4262ae188f59c7fecdbc17a1a252addf3841aede
MarieEchard <marie.echard79@gmail.com>
2024-04-15 20:48:07 +0200

    rectification Version Doctrine

--- migrations/Version20240415135935.php
+++ migrations/Version20240415135935.php
@@ -21,6 +21,7 @@ final class Version20240415135935 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
 
+        $this->addSql('ALTER TABLE projet ADD CONSTRAINT FK_50159CA9BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
         $this->addSql('ALTER TABLE question CHANGE question question VARCHAR(1000) NOT NULL, CHANGE reponse reponse VARCHAR(1000) DEFAULT NULL');
         $this->addSql('ALTER TABLE user_projet ADD CONSTRAINT FK_35478794A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE user_projet ADD CONSTRAINT FK_35478794C18272 FOREIGN KEY (projet_id) REFERENCES projet (id) ON DELETE CASCADE');
@@ -33,8 +34,8 @@ final class Version20240415135935 extends AbstractMigration
         $this->addSql('ALTER TABLE question CHANGE question question VARCHAR(255) NOT NULL, CHANGE reponse reponse VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE rendez_vous DROP FOREIGN KEY FK_65E8AA0AC18272');
         $this->addSql('ALTER TABLE rendez_vous DROP FOREIGN KEY FK_65E8AA0AA76ED395');
-        $this->addSql('ALTER TABLE projet_user DROP FOREIGN KEY FK_35478794A76ED395');
-        $this->addSql('ALTER TABLE projet_user DROP FOREIGN KEY FK_35478794C18272');
+        $this->addSql('ALTER TABLE user_projet DROP FOREIGN KEY FK_35478794A76ED395');
+        $this->addSql('ALTER TABLE user_projet DROP FOREIGN KEY FK_35478794C18272');
         $this->addSql('ALTER TABLE projet DROP FOREIGN KEY FK_50159CA9BCF5E72D');
         $this->addSql('ALTER TABLE item_portfolio_categorie DROP FOREIGN KEY FK_283EF0DA7631AB82');
         $this->addSql('ALTER TABLE item_portfolio_categorie DROP FOREIGN KEY FK_283EF0DABCF5E72D');
