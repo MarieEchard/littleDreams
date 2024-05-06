8559a0daa5468bb55ddc55759946aa170cc6b23e
MarieEchard <marie.echard79@gmail.com>
2024-05-06 18:00:48 +0200

    Mise a jour version

--- migrations/Version20240503143244.php
+++ migrations/Version20240503143244.php
@@ -20,7 +20,6 @@ final class Version20240503143244 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE projet CHANGE user_id user_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE projet ADD CONSTRAINT FK_50159CA9BCF5E72D FOREIGN KEY (categorie_id) REFERENCES categorie (id)');
         $this->addSql('ALTER TABLE projet ADD CONSTRAINT FK_50159CA9A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494EA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
